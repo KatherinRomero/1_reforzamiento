270130b16a0635c6a7895bd77f8add1586a628ca
Kathe <katherinvanessa16@gmail.com>
2026-01-16 11:58:30 -0500

    interfaces

--- src/bases/03-object-literals.ts
+++ src/bases/03-object-literals.ts
@@ -1,8 +1,7 @@
-
 const ironman ={
     firstName:"Tony",
     lastName:"Stark",
-    ege:45,
+    age:45,
     address:{
         postalCode:"ABC123",
         city:"New York"
@@ -12,14 +11,35 @@ const spiderman ={...ironman}
 //{...objeto} se usa para clonar un objeto solo las propiedades de primer nivel
 spiderman.firstName="Peter"
 spiderman.lastName="Parker"
-spiderman.ege=22
+spiderman.age=22
 
 const cap =structuredClone(ironman)
 //structuredClone(objeto) se usa para clonar un objeto incluyendo las propiedades anidadas
 cap.firstName="steve"
 cap.lastName=" rogers"
-cap.ege=27
+cap.age=27
 cap.address.city=" Washington D.C."
 
 console.log(ironman, spiderman,cap)
 
+//nterfaces
+interface Person{ //la interfas es estructura de un objeto
+    firstName:string,
+    lastName:string,
+    age:number
+    adress?:Address//el signo de interrogacion indica que la propiedad es opcional
+}
+
+   interface Address {
+        postalCode:string,
+        city:string,
+    }
+
+
+const hulk:Person={
+    firstName: "Bruce",
+    lastName: "Banner",
+    age: 45
+}
+
+console.log(hulk)
